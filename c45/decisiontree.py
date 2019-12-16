c8f6e76bf0210d84d8230617371a0a08012ad611
Somainer <somainer@gmail.com>
2019-12-16 23:57:08 +0800

    Update decisiontree.py

--- c45/decisiontree.py
+++ c45/decisiontree.py
@@ -10,14 +10,39 @@ T = TypeVar('T')
 
 
 class DTree:
-    def __init__(self, label: T, threshold: Optional[Numeric] = None):
+    def __init__(self, label: T, threshold: Optional[Numeric] = None, children: Optional[list] = None):
         self.label = label
         self.threshold = threshold
         self.isLeaf = threshold is None
-        self.children = []
+        if children:
+            l, r = children
+            if l.is_leaf() and r.is_leaf() and l.label == r.label:
+                self.children = []
+                self.label = l.label
+                self.threshold = None
+            else:
+                self.children = children
+        else:
+            self.children = []
+
+    def is_leaf(self):
+        return not self.children
 
     def to_string(self) -> str:
         return f'Tree({self.label}, {self.threshold}, [{"".join(map(DTree.to_string, self.children))}])'
+    
+    def format_tree(self, level: int = 0):
+        s = '  ' * level
+        if self.is_leaf():
+            return f'{s}{self.label}'
+        return f'if [{self.label}] < {self.threshold}:\n{s} {self.children[0].format_tree(level + 1)}\n{s}else:\n{s} {self.children[1].format_tree(level + 1)}'
+
+    def predict(self, data: list):
+        if self.is_leaf():
+            return self.label
+        if data[self.label] < self.threshold:
+            return self.children[0].predict(data)
+        return self.children[1].predict(data)
 
     def __str__(self):
         return self.to_string()
@@ -51,10 +76,9 @@ class C45Algo:
         if all_same_class:
             return DTree(current_data[0][-1])
 
-        gain, attribute, threshold, split = max(self.try_split(current_data, current_attribute), key=itemgetter(0))
+        _gain, attribute, threshold, split = max(self.try_split(current_data, current_attribute), key=itemgetter(0))
         remaining = [att for att in current_attribute if att != attribute]
-        node = DTree(attribute, threshold)
-        node.children = [self.generate_tree(subset, remaining) for subset in split]
+        node = DTree(attribute, threshold, [self.generate_tree(subset, remaining) for subset in split])
         return node
 
     @classmethod
@@ -100,4 +124,4 @@ class C45Algo:
 
         counter = Counter(map(itemgetter(-1), data))
         possibilities = map(lambda x: x / length, counter.values())
-        return -sum(map(lambda x: x * cls.log(x), possibilities))
+        return - sum(map(lambda x: x * cls.log(x), possibilities))
